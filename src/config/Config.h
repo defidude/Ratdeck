37b536ea7f90c71ca438ab350e8bb4338a17844b
DeFiDude <59237470+DeFiDude@users.noreply.github.com>
2026-04-26 23:54:53 -0600

    v1.8.4-beta: prerelease build
    Mark prerelease tags (containing '-') as GitHub Pre-release.

--- src/config/Config.h
+++ src/config/Config.h
@@ -6,8 +6,8 @@
 
 #define RATDECK_VERSION_MAJOR  1
 #define RATDECK_VERSION_MINOR  8
-#define RATDECK_VERSION_PATCH  3
-#define RATDECK_VERSION_STRING "1.8.3"
+#define RATDECK_VERSION_PATCH  4
+#define RATDECK_VERSION_STRING "1.8.4-beta"
 
 // --- Feature Flags ---
 #define HAS_DISPLAY     true
